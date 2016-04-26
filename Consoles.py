9090bea6f7d7749b343601072709f78cedc81cda
bonattt <bonattt@rose-hulman.edu>
2016-04-26 13:24:43 -0400

    started working

---
+++ Consoles.py
@@ -0,0 +1,24 @@
+
+
+class AbstractConsole():
+
+    def __init__(self):
+        pass
+
+
+class NullConsole(AbstractConsole):
+
+    def run_console(self):
+        pass
+
+    def __init__(self):
+        pass
+
+
+class CombatConsole(AbstractConsole):
+
+    def __init__(self):
+        pass
+
+    def run_console(self):
+        pass
